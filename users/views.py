3f4a5252acb4159eeac22651868eb5ba0b4f6ba6
joycem8845 <theeirishmonsta@gmail.com>
2018-10-15 21:06:40 -0500

    added urls and view to users and edited base.html

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import logout
 
 # Create your views here.
+
+def logout_view(request):
+	"""log out"""
+	logout(request)
+	return HttpResponseRedirect(reverse('zoo_animal_feeders:index'))
