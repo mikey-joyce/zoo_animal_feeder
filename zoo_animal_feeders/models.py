0abd96ba9d7db6f232ebdaf8105ca9a2ef7c6575
Mikey Joyce <joycem8845@parkwayschools.net>
2018-10-24 20:04:43 -0500

    Debugged

--- zoo_animal_feeders/models.py
+++ zoo_animal_feeders/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
 
 # Create your models here.
 
@@ -7,7 +8,7 @@ class AnimalType(models.Model):
     """Type of animal that can classify the animal"""
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     a_type = models.CharField(max_length=50, default='')
-    date_added = models.DateField(auto_now_add=True)
+    date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.a_type
@@ -16,7 +17,7 @@ class Animal(models.Model):
     """The actual animal, embeded in animaltype"""
     animal_type = models.ForeignKey(AnimalType, on_delete=models.CASCADE)
     name = models.CharField(max_length=60, default='')
-    date_added = models.DateField(auto_now_add=True)
+    date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         if len(self.name)>20:
